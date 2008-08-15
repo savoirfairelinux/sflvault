bb5b55f3349904e730ce5b4d69197cbedb085419
Alexandre Bourget <alexandre.bourget@savoirfairelinux.com>
2008-08-15 11:23:22 -0400

    xmlrpc: Speed up login by lazyloading user ciphers (on obj. User)

--- sflvault/model/__init__.py
+++ sflvault/model/__init__.py
@@ -164,7 +164,7 @@ class Customer(object):
 # Map each class to its corresponding table.
 mapper(User, users_table, {
     'levels': relation(UserLevel, backref='user', lazy=False),
-    'ciphers': relation(Usercipher, backref='user', lazy=False)
+    'ciphers': relation(Usercipher, backref='user', lazy=True)
     })
 mapper(UserLevel, userlevels_table, {
     
