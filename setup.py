8d5394d833e35272f31929c78c73fd086906446d
Alexandre Bourget <alexandre.bourget@savoirfairelinux.com>
2009-02-12 15:40:53 -0500

    Release 0.7.1dev, read previous commit for release notes

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except ImportError:
 
 setup(
     name='SFLvault',
-    version="0.7.0",
+    version="0.7.1",
     description='Networked credentials store and authentication manager',
     author='Alexandre Bourget',
     author_email='alexandre.bourget@savoirfairelinux.com',
