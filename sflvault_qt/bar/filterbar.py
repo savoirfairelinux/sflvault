8364f214a3467213cc17bb97d9a9457aa90839c6
titilambert <titilambert@Kangourex.(none)>
2009-06-07 12:02:11 -0400

    Put filter bar in bar folder

---
+++ sflvault_qt/bar/filterbar.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import sys
+from PyQt4 import QtCore, QtGui
+import re
+from PyQt4.QtCore import Qt
+import sflvault
+from sflvault.client import SFLvaultClient
+from images.qicons import *
+import shutil
+import os
+
+
+class FilterBar(QtGui.QWidget):
+    def __init__(self, parent=None):
+        QtGui.QWidget.__init__(self, parent)
+        self.parent = parent
+
+        layout = QtGui.QHBoxLayout(self)
+        layout.setMargin(0)
+
+        self.close = QtGui.QToolButton(self);
+        self.close.setAutoRaise(True);
+        self.close.setIcon(Qicons("close_filter_icon"))
+        self.close.setToolTip(self.tr("Hide Filter Bar"))
+        layout.addWidget(self.close);
+
+        self.filter_label = QtGui.QLabel(self.tr("Filter :"));
+        layout.addWidget(self.filter_label);
+
+        self.filter_input = QtGui.QLineEdit(self)
+        self.filter_input.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.filter_label.setBuddy(self.filter_input);
+        layout.addWidget(self.filter_input);
+
