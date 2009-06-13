7b34eb2f6bb6506d4589f9b485746fa5ca1c9f1f
titilambert <titilambert@Kangourex.(none)>
2009-06-13 17:38:20 -0400

    add lib folder

---
+++ lib/auth.py
@@ -0,0 +1,92 @@
+# -*- coding: UTF-8 -*-
+
+import sys
+from PyQt4 import QtCore, QtGui
+from sflvault.client import SFLvaultClient
+
+from lib.error import *
+token = None
+
+def getAuth():
+    """
+        Get authentication
+    """
+    global token
+    if not token:
+        token = SFLvaultClient()
+        token.search("[a]")
+#        vaultSearch("[a]")
+    return token
+    
+def getService(id):
+    global token
+    try:
+        service = token.vault.service.get(token.authtok, id)
+    except Exception, e:
+        ErrorMessage(e)
+    return service
+
+def getMachine(id):
+    global token
+    try:
+        machine = token.vault.machine.get(token.authtok, id)
+    except Exception, e:
+        ErrorMessage(e)
+    return machine
+
+def getCustomer(id):
+    global token
+    try:
+        customer = token.vault.customer.get(token.authtok, id)
+    except Exception, e:
+        ErrorMessage(e)
+    return customer
+
+def vaultSearch(pattern, groups_ids=None):
+    global token
+    result = None
+    try:
+        result = token.vault.search(token.authtok, pattern, groups_ids)
+    except Exception, e:
+        ErrorMessage(e)
+    return result
+
+def getPassword(id):
+    global token
+    password = None
+    try:
+        password = token.service_get(id)["plaintext"]
+    except Exception, e:
+        Errormessage(e)
+    return password
+
+def getUserList():
+    global token
+    users = None
+    try:
+        users = token.vault.user_list(token.authtok, True)["list"]
+    except Exception, e:
+        ErrorMessage(e)
+    return users
+
+def getGroupList():
+    global token
+    groups = None
+    try:
+        groups = token.vault.group_list(token.authtok)["list"]
+    except Exception, e:
+        ErrorMessage(e)
+    return groups
+
+
+token_alias = SFLvaultClient()
+
+def getAliasList():
+    aliases = token_alias.alias_list()
+    return aliases
+
+def saveAlias(alias, id):
+    token_alias.alias_add(alias,id)
+
+def delAlias(alias):
+    token_alias.alias_del(alias)
---
+++ lib/error.py
@@ -0,0 +1,28 @@
+# -*- coding: UTF-8 -*-
+
+import sys
+from PyQt4 import QtCore, QtGui
+from sflvault.client import SFLvaultClient
+from images.qicons import *
+
+
+class ErrorMessage(QtGui.QMessageBox):
+    def __init__(self, exception, parent=None):
+        QtGui.QMessageBox.__init__(self, parent)
+        self.exception = exception
+        #if self.exception.message[0] == 111:
+        #    self.connectionError()
+        #else:
+        self.message()
+        self.exec_()
+        
+
+    def connectionError(self):
+        self.setWindowTitle(self.tr("Connection Error"))
+        self.setText(self.tr(self.exception[1]))
+        self.setIcon(QtGui.QMessageBox.Critical)
+
+    def message(self):
+        self.setWindowTitle(self.tr("Connection"))
+        self.setText(self.tr("qsfqdg"))
+        self.setIcon(QtGui.QMessageBox.Critical)
