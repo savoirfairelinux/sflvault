b1593a8688a6461af2db80d6aa5138b7c1918ec6
Alexandre Bourget <alexandre.bourget@savoirfairelinux.com>
2010-07-16 09:36:08 -0400

    [Cosmetics]

--- client-qt/sflvault/clientqt/gui/wizard/initaccount.py
+++ client-qt/sflvault/clientqt/gui/wizard/initaccount.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: UTF-8 -*-
+# -*- coding: utf-8 -*-
 #
 #    sflvault_qt/wizard/initaccount.py
 #
@@ -83,7 +83,7 @@ class Page2(QtGui.QWizardPage):
 
         address_label = QtGui.QLabel(self.tr("&Vault URL"))
         self.address = QtGui.QLineEdit()
-        self.address.setText("http://localhost:5000/vault/rp")
+        self.address.setText("https://localhost:5000/vault/rpc")
         address_label.setBuddy(self.address)
 
         password1_label = QtGui.QLabel(self.tr("&Password"))
