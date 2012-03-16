07e2d1587f96b31848943e5c8fc8678281c58a8b
Joao Alfredo Gama Batista <joao.gama@savoirfairelinux.com>
2012-03-16 12:14:38 -0400

    Typo error.

--- server/sflvault/model/meta.py
+++ server/sflvault/model/meta.py
@@ -19,7 +19,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-g"""SQLAlchemy Metadata and Session object"""
+"""SQLAlchemy Metadata and Session object"""
 from sqlalchemy import MetaData
 
 __all__ = ['engine', 'metadata', 'Session']
