11e5a40d3cd77d121e94aa938e0e7353a8d65aee
Lionel Nicolas <lionel.nicolas@savoirfairelinux.com>
2014-05-01 23:58:32 -0400

    Update sflvault-client build setup script

--- client/setup.py
+++ client/setup.py
@@ -19,58 +19,50 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-try:
-    from setuptools import setup, find_packages
-except ImportError:
-    from distribute_setup import use_setuptools
-    use_setuptools()
-    from setuptools import setup, find_packages
+from setuptools import setup, find_packages
 
 import platform
 
+requires = [
+    "sflvault-common",
+    "pycrypto",
+    "decorator"
+]
+
+if platform.system() != "Windows":
+    requires += [
+        "urwid",
+        "pexpect-u"
+    ]
 
 setup(
-    name='SFLvault-client',
-    version="0.7.8.2",
-    description='Networked credentials store and authentication manager - Client',
-    author='Alexandre Bourget',
-    author_email='alexandre.bourget@savoirfairelinux.com',
-    url='http://www.sflvault.org',
-    license='GPLv3',
-    install_requires=["SFLvault-common==0.7.8.1",
-                      "pycrypto",
-                      "decorator",
-                      ] + \
-                     ([] if platform.system() == 'Windows'
-                         else ["urwid>=0.9.8.1",
-                               "pexpect-u>=2.5"]),
-    packages=find_packages(),
-    namespace_packages=['sflvault'],
-    include_package_data=True,
-    test_suite='nose.collector',
-    package_data={'sflvault': ['i18n/*/LC_MESSAGES/*.mo']},
-    #message_extractors = {'sflvault': [
-    #        ('**.py', 'python', None),
-    #        ('templates/**.mako', 'mako', None),
-    #        ('public/**', 'ignore', None)]},
-    entry_points="""
-    [console_scripts]
-    sflvault = sflvault.client.commands:main
+    name               = "sflvault-client",
+    version            = "0.8.0",
+    description        = "Networked credentials store and authentication manager - Client",
+    author             = "Alexandre Bourget",
+    author_email       = "alexandre.bourget@savoirfairelinux.com",
+    url                = "http://www.sflvault.org",
+    license            = "GPLv3",
+    packages           = find_packages(),
+    install_requires   = requires,
+    namespace_packages = ["sflvault"],
+    entry_points       = """
+        [console_scripts]
+        sflvault = sflvault.client.commands:main
 
-    [sflvault.services]
-    ssh = sflvault.client.services:ssh
-    ssh+pki = sflvault.client.services:ssh_pki
-    content = sflvault.client.services:content
-    sflvault = sflvault.client.services:sflvault
-    vnc = sflvault.client.services:vnc
-    mysql = sflvault.client.services:mysql
-    psql = sflvault.client.services:postgres
-    postgres = sflvault.client.services:postgres
-    postgresql = sflvault.client.services:postgres
-    su = sflvault.client.services:su
-    sudo = sflvault.client.services:sudo
+        [sflvault.services]
+        ssh = sflvault.client.services:ssh
+        ssh+pki = sflvault.client.services:ssh_pki
+        content = sflvault.client.services:content
+        sflvault = sflvault.client.services:sflvault
+        vnc = sflvault.client.services:vnc
+        mysql = sflvault.client.services:mysql
+        psql = sflvault.client.services:postgres
+        postgres = sflvault.client.services:postgres
+        postgresql = sflvault.client.services:postgres
+        su = sflvault.client.services:su
+        sudo = sflvault.client.services:sudo
 
-    """,
+    """
 )
-
 
