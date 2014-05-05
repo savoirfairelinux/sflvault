fd487be2a1157bae3e52c2077f002795a98ee1ff
Lionel Nicolas <lionel.nicolas@savoirfairelinux.com>
2014-05-05 17:32:26 -0400

    Update sflvault-client-qt build setup script

--- client-qt/setup.py
+++ client-qt/setup.py
@@ -1,36 +1,55 @@
-try:
-    from setuptools import setup, find_packages
-except ImportError:
-    from distribute_setup import use_setuptools
-    use_setuptools()
-    from setuptools import setup, find_packages
+# -=- encoding: utf-8 -=-
+#
+# SFLvault - Secure networked password store and credentials manager.
+#
+# Copyright (C) 2008-2014 Savoir-faire Linux inc.
+#
+# Author: Alexandre Bourget <alexandre.bourget@savoirfairelinux.com>
+# Author: Lionel Nicolas <lionel.nicolas@savoirfairelinux.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+from setuptools import setup, find_packages
 
+import glob
+
+requires = [
+    "sflvault-common",
+    "sflvault-client"
+]
 
 setup(
-    name='SFLvault-client-qt',
-    version="0.7.8.2",
-    description='Networked credentials store and authentication manager - Qt Client',
-    author='Thibault Cohen',
-    author_email='thibault.cohen@savoirfairelinux.com',
-    url='http://www.sflvault.org',
-    license='GPLv2',
-    install_requires=[
-                    #"PyQt",
-                      "SFLvault-client==0.7.8.2",
-                      "SFLvault-common==0.7.8.1",
-                      ],
-    include_package_data=True,
-    packages=find_packages(exclude=['ez_setup']),
-    test_suite='nose.collector',
-    #message_extractors = {'sflvault': [
-    #        ('**.py', 'python', None),
-    #        ('templates/**.mako', 'mako', None),
-    #        ('public/**', 'ignore', None)]},
-    entry_points="""
-    [console_scripts]
-    sflvault-client-qt = sflvault.clientqt:main
+    name               = "sflvault-client-qt",
+    version            = "0.8.0",
+    description        = "Networked credentials store and authentication manager - Qt Client",
+    author             = "Thibault Cohen",
+    author_email       = "thibault.cohen@savoirfairelinux.com",
+    url                = "http://www.sflvault.org",
+    license            = "GPLv3",
+    packages           = find_packages(),
+    install_requires   = requires,
+    namespace_packages = ["sflvault"],
+    test_suite         = "nose.collector",
+    entry_points       = """
+        [console_scripts]
+        sflvault-client-qt = sflvault.clientqt:main
     """,
+    data_files         = [
+        # For the moment, /usr/share/pyshared/sflvault/clientqt is hardcoded before finding a solution
+        ("/usr/share/pyshared/sflvault/clientqt/images",          glob.glob("sflvault/clientqt/images/*png")),
+        ("/usr/share/pyshared/sflvault/clientqt/images/services", glob.glob("sflvault/clientqt/images/services/*png")),
+        ("/usr/share/pyshared/sflvault/clientqt/i18n",            glob.glob("sflvault/clientqt/i18n/*qm"))
+	]
 )
-
 
