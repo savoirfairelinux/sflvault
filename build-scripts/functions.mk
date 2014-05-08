874ceabbeb5ce56e9598dc589b8159b62c8c1310
Lionel Nicolas <lionel.nicolas@savoirfairelinux.com>
2014-05-08 19:48:05 -0400

    Add initial RPM makefile functions

--- build-scripts/functions.mk
+++ build-scripts/functions.mk
@@ -5,6 +5,7 @@ define package-clean
 endef
 
 define package-distclean
+	rm -rf $(DIR_RPM)/$(PKG_PREFIX)$(1)
 	rm -rf $(DIR_DEB)/$(PKG_PREFIX)$(1)
 endef
 
@@ -16,6 +17,18 @@ define package-deb-src
 	mkdir -pv $(DIR_DEB)/$(PKG_PREFIX)$(1)
 	cp -vf $(1)/deb_dist/$(PKG_PREFIX)$(1)*.tar.gz $(DIR_DEB)/$(PKG_PREFIX)$(1)/
 	cp -vf $(1)/deb_dist/$(PKG_PREFIX)$(1)*.dsc    $(DIR_DEB)/$(PKG_PREFIX)$(1)/
+endef
+
+define package-rpm-src
+	cd $(1) && python setup.py bdist_rpm --spec-only
+	cd $(1) && python setup.py sdist
+	mkdir -pv $(DIR_RPM)/$(PKG_PREFIX)$(1)
+	cp -vf $(1)/dist/$(PKG_PREFIX)$(1)*.tar.gz $(DIR_RPM)/$(PKG_PREFIX)$(1)/
+	cp -vf $(1)/dist/$(PKG_PREFIX)$(1).spec    $(DIR_RPM)/$(PKG_PREFIX)$(1)/
+endef
+
+define package-rpm-bin
+	@echo "Not implemented"
 endef
 
 define package-deb-bin
